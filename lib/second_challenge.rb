554f32f29da9afda0083b9d8291f465d74f0e637
Ally Morson <ally.morson@gmail.com>
2019-09-03 20:33:23 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,12 @@ def second_challenge
   epic_tragedy = {
     :montague => {
      :patriarch => {
-     
+        },
+      :matriarch => {
+        },
+      :hero => {
+        },
+      :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
